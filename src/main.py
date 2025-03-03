cf42654a1a3edb2873438028174197edf61d1b57
cobinding <cntpqls0218@gmail.com>
2025-03-03 19:31:37 +0900

    feature: get 요청과 to-do 리스트 반환(DB 없이 딕셔너리로 데이터 정의)

--- src/main.py
+++ src/main.py
@@ -4,3 +4,30 @@ app = FastAPI()
 @app.get("/")
 def health_check_handler():
     return {"ping":"pong"}
+
+
+# DB 대신 딕셔너리로 데이터 관리
+# return 값
+todo_data = {
+    1: {
+        "id": 1,
+        "contents": "실전! FastAPI 섹션 0 수강",
+        "is_done": True,
+    },
+    2: {
+        "id": 2,
+        "contents": "실전! FastAPI 섹션 1 수강",
+        "is_done": False,
+    },
+    3: {
+        "id": 3,
+        "contents": "실전! FastAPI 섹션 2 수강",
+        "is_done": False,
+    }
+}
+
+@app.get("/todos")
+def get_todos_handler() -> list:
+    return list(todo_data.values())
+
+
