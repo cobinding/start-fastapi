047a035b9957e782ef16eb3d2ac64b0cd5eabab4
cobinding <cntpqls0218@gmail.com>
2025-03-03 22:42:24 +0900

    feature: post 요청과 Request Body

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 
 @app.get("/")
@@ -41,3 +42,22 @@ def get_todos_handler(order: str | None = None):
     if order == "DESC":
         return result[::-1]
     return result
+
+# ID를 통한 상세 조회
+@app.get("/todos/{todo_id}")
+def get_todo_handler(todo_id: int):
+    return todo_data.get(todo_id, {})
+
+# post API로 todo 생성
+# `pydentic` 모듈로 쉽게 Request Body 처리
+
+# request record 값
+class CreateTodoRequest(BaseModel):
+    id: int
+    contents: str
+    is_done: bool
+
+@app.post("/todos")
+def create_todo_handler(request: CreateTodoRequest):
+    todo_data[request.id] = request.dict()
+    return todo_data[request.id]
