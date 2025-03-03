da8e74870a7d08403c2aede686548d595ed76913
cobinding <cntpqls0218@gmail.com>
2025-03-03 22:30:09 +0900

    feature: get 요청과 query parameter

--- src/main.py
+++ src/main.py
@@ -26,8 +26,18 @@ todo_data = {
     }
 }
 
-@app.get("/todos")
+@app.get("/")
 def get_todos_handler() -> list:
     return list(todo_data.values())
 
-
+"""Query Parameter(/todos?query=apple)
+직접 작성하지 않고, 인자를 통해 정의
+"""
+@app.get("/todos")
+# 쿼리 파라미터 값을 Optional로 지정,
+# default = None
+def get_todos_handler(order: str | None = None):
+    result = list(todo_data.values())
+    if order == "DESC":
+        return result[::-1]
+    return result
